d15cd9136827202ec3b318a44b7a0b85be614be7
Lirosk <kirill_crysis@bk.ru>
2021-06-09 22:43:28 +0300

    l2

--- l2/setup.py
+++ l2/setup.py
@@ -4,4 +4,6 @@ setup(
    name='serializer',
    version='1.0',
    description='A useful module',
+   author='Egor Isichenko', # God blessing
+   author_email='eisichenko@main.ru',
    packages=['serializer'])
